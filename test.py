838271a101a29f77cc47fab0d985534073fb429f
Changhong Wang <changhong.wang@telecom-paris.fr>
2023-04-14 00:26:12 +0200

    irmas only final

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from deem import deem
 
 
-param_grid = {'LR_param': {'C':[10**k for k in range(-10, 6, 2)]}, 'scoring': 'roc_auc', 'cv': 3, 'random_state': 42}
+param_grid = {'LR_param': {'C':[10**k for k in range(-8, 4, 1)]}, 'scoring': 'roc_auc', 'cv': 3, 'random_state': 42}
 
 with open("genre_map.json", "r") as f: # only consider 10 classes of Openmic dataset
     genre_map = json.load(f)
@@ -34,15 +34,14 @@ with open("instrument_map.json", "r") as f: # only consider 10 classes of Openmi
     instrument_map = json.load(f)
 
 embedding = 'vggish'
-debias_method = ''
+debias_method = '-mklda'
 
 deb = deem(embedding = embedding, debias_method = debias_method, feature_dir='./embeddings.h5', 
            instrument_map=instrument_map, genre_map=genre_map, param_grid=param_grid)
 
-A_meta = pd.read_csv("A_meta.csv")
-A_feature = deb.load_feature(A_meta)
-B_meta = pd.read_csv("B_meta.csv")
-B_feature = deb.load_feature(B_meta)
+meta_all = pd.read_csv("irmas_meta_all.csv")
+A_feature = deb.load_feature(meta_all, 'A')
+B_feature = deb.load_feature(meta_all, 'B')
 
 deb.instrument_classfication(train_set='A', test_set='A', A_feature=A_feature, B_feature=B_feature)
 deb.instrument_classfication(train_set='A', test_set='B', A_feature=A_feature, B_feature=B_feature)
